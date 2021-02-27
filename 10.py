bf9de510c6487bf8b4885947b8686b462720da1c
Diosretes <dozsaferenc@gmail.com>
2021-02-27 13:26:50 +0100

    .

--- 10.py
+++ 10.py
@@ -2,6 +2,15 @@
 lista1 = ['Xena','Bozsi','Vica','Eni','Ildi' ]
 
 print(lista1)
+print(lista1[0])
 
-lista1.sort()
-print(lista1)
+
+print(lista1[-2])
+
+lista2 = [(1,2.3), (4,5,6), (7,8,9)]
+print(lista2)
+
+print(lista2[2][0])
+
+tartomany = range(100)
+print(list(tartomany))
